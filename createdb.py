6198929f8919e813cd81235c4e0c22761b24f8ca
Thanassis Tsiodras <ttsiodras@gmail.com>
2021-09-28 16:42:09 +0200

    Optimising. Now running 3x faster than before.

--- createdb.py
+++ createdb.py
@@ -4,7 +4,6 @@ import re
 import sys
 import time
 import queue
-import logging
 import multiprocessing
 from collections import defaultdict
 import pickle
@@ -13,7 +12,6 @@ from typing import List, Any
 
 
 def process_file(producer_Q, consumer_Q):
-    s = re.compile(r'^([^:]*):(.*)')
     t = re.compile(r'^.*?href="([^"].*?)"(.*)')
     u = re.compile(r'^(.*)/[^/]*$')
     v = re.compile(r'^(.*)#.*$')
@@ -23,45 +21,30 @@ def process_file(producer_Q, consumer_Q):
         work_item = consumer_Q.get()
         filename = work_item
         if filename == "":
-            logging.info("[-] We are instructed to abort. Worker exiting...")
+            # print("\n[-] Worked instructed to abort. Worker exiting...")
             break
-        full_path = filename
-        # logging.info("[-] Processing: %s", full_path)
-
-        cmd = "grep href \"" + filename + "\" /dev/null"
-        for line in os.popen(cmd).readlines():
-            line = line.strip()
-            m = s.match(line)
-            if m:
-                source = m.group(1)
-                if not os.path.exists(source):
-                    continue
-                source = os.path.realpath(source)
-                folder = u.match(source).group(1)
-                m = t.match(m.group(2))
-                while m:
-                    link = m.group(1)
-                    if link.startswith('#'):
-                        m = t.match(m.group(2))
-                        continue
-                    q = v.match(link)
-                    if q:
-                        link = q.group(1)
-                    realp = os.path.realpath(folder + os.sep + link)
-                    if os.path.exists(realp):
-                        references[source].add(realp)
+        folder = u.match(filename).group(1)
+        for line in open(filename):
+            if 'href' not in line:
+                continue
+            m = t.match(line)
+            while m:
+                link = m.group(1)
+                if link.startswith('#'):
                     m = t.match(m.group(2))
-        logging.info(
-            "[-] End of processing: %s, remaining items: %d",
-            full_path, consumer_Q.qsize())
+                    continue
+                q = v.match(link)
+                if q:
+                    link = q.group(1)
+                realp = os.path.realpath(folder + os.sep + link)
+                if os.path.exists(realp):
+                    references[filename].add(realp)
+                m = t.match(m.group(2))
+        print("\r[-] Remaining: %6d" % consumer_Q.qsize(), end='')
     producer_Q.put(references)
 
 
 def main():
-    if '-v' in sys.argv:
-        logging.basicConfig(level=logging.INFO)
-        sys.argv.remove("-v")
-
     if len(sys.argv) != 2:
         print("Usage: " + sys.argv[0] + " <folderWithHTML>")
         sys.exit(1)
@@ -76,15 +59,16 @@ def main():
     producer_Q = multiprocessing.Queue()  # type: Any
     consumer_Q = multiprocessing.Queue()  # type: Any
 
+    total = len(all_html_files)
+    print("[-] Queueing %d HTML files..." % total)
+
     for unused in range(multiprocessing.cpu_count()):
         proc = multiprocessing.Process(
             target=process_file, args=(producer_Q, consumer_Q))
         list_of_processes.append(proc)
         proc.start()
 
-    total = len(all_html_files)
     for idx, f in enumerate(all_html_files):
-        logging.info("[-] Queued %s (%d/%d)...", f, idx+1, total)
         consumer_Q.put(f)
 
     for proc in list_of_processes:
@@ -96,9 +80,10 @@ def main():
     for idx, proc in enumerate(list_of_processes):
         while True:
             try:
-                logging.info("[-] Waiting for result from worker %d", idx)
                 references_computed = producer_Q.get()
-                logging.info("[-] Obtained result from worker %d", idx)
+                if idx == 0:
+                    print("")
+                print("[-] Obtained result from worker %d" % idx)
                 for k, v_list in references_computed.items():
                     for v in v_list:
                         references[k].add(v)
